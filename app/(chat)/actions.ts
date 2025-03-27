5b1b6453bd92f8841dfdb38873eb694b9b3e45d1
Sergei Khmylov <sergei@khmylov.me>
2025-03-27 23:58:35 +0400

    updated title prompt for the russian language

--- app/(chat)/actions.ts
+++ app/(chat)/actions.ts
@@ -24,10 +24,11 @@ export async function generateTitleFromUserMessage({
   const { text: title } = await generateText({
     model: myProvider.languageModel('title-model'),
     system: `\n
-    - you will generate a short title based on the first message a user begins a conversation with
-    - ensure it is not more than 80 characters long
-    - the title should be a summary of the user's message
-    - do not use quotes or colons`,
+    - Вы будете генерировать короткое название на основе первого сообщения, с которого пользователь начинает разговор.
+    - убедитесь, что его длина не превышает 80 символов
+    - заголовок должен быть кратким изложением сообщения пользователя
+    - не используйте кавычки и двоеточия
+    - заголовок должен быть на русском языке`,
     prompt: JSON.stringify(message),
   });
 
