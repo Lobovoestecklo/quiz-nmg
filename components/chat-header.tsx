834a1a97884e8ef4557fff8c7f3bf1ba6f31e1be
Sergei Khmylov <sergei@khmylov.me>
2025-04-21 17:25:09 +0200

    made visibility selector hidden for now

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -63,13 +63,13 @@ function PureChatHeader({
         />
       )} */}
 
-      {!isReadonly && (
+      {/* {!isReadonly && (
         <VisibilitySelector
           chatId={chatId}
           selectedVisibilityType={selectedVisibilityType}
           className="order-1 md:order-3"
         />
-      )}
+      )} */}
     </header>
   );
 }
