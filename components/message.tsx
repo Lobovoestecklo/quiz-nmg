ba2bc5759913be23736f405a6945d1421af8fd28
Sergei Khmylov <sergei@khmylov.me>
2025-05-21 17:12:38 +0200

    i18n + insert suggestion comp draft

--- components/message.tsx
+++ components/message.tsx
@@ -112,7 +112,7 @@ const PurePreviewMessage = ({
                               <PencilEditIcon />
                             </Button>
                           </TooltipTrigger>
-                          <TooltipContent>Edit message</TooltipContent>
+                          <TooltipContent>Редактировать</TooltipContent>
                         </Tooltip>
                       )}
 
@@ -231,19 +231,27 @@ const PurePreviewMessage = ({
                 isLoading={isLoading}
               />
             )}
-            <div
-              key={'test-iddddd'}
-              className="flex flex-row gap-2 items-start"
-            >
+            <div key={'test-iddddd'} className="relative">
               <div
-                data-testid="message-content"
-                className={cn(
-                  'flex flex-col gap-4 p-4 border rounded-2xl flex flex-row gap-2 items-center justify-between dark:bg-muted dark:border-zinc-700',
-                  {
-                    'bg-primary text-primary-foreground px-3 py-2 rounded-xl': false,
-                  },
-                )}
+                className="size-full absolute top-0 left-0 rounded-xl z-10"
+                aria-hidden="true"
               >
+                <div className="w-full p-4 flex justify-end items-center">
+                  <div className="absolute right-[9px] top-[13px] p-2 hover:dark:bg-zinc-700 rounded-md hover:bg-zinc-100">
+                    fullscr icon
+                  </div>
+                </div>
+              </div>
+              <div className="p-4 border rounded-t-2xl flex flex-row gap-2 items-start sm:items-center justify-between dark:bg-muted border-b-0 dark:border-zinc-700">
+                <div className="flex flex-row items-start sm:items-center gap-3">
+                  <div className="text-muted-foreground">icon</div>
+                  <div className="-translate-y-1 sm:translate-y-0 font-medium">
+                    title
+                  </div>
+                </div>
+                <div className="w-8" />
+              </div>
+              <div className="border rounded-b-2xl dark:bg-muted border-t-0 dark:border-zinc-700 p-4 sm:px-4 sm:py-4">
                 <Markdown>
                   Какой-то текст длинный текст Какой-то текст длинный текст
                   Какой-то текст длинный текст Какой-то текст длинный текст
